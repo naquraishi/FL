d44a9aefdb50c2c529ac0037bdc0f70401e1812c
Nafeez Quraishi <nafeez.quraishi@pan-net.eu>
2018-11-05 19:24:19 +0100

    alert handling in test

--- src/test/java/tests/AddNichePerfumeTest.java
+++ src/test/java/tests/AddNichePerfumeTest.java
@@ -37,6 +37,9 @@ public class AddNichePerfumeTest extends FluentLeniumTest {
         nischenDueftePage.niedrigsterOptionSelection();
         String itemDetails =  nischenDueftePage.addItemName();
         nischenDueftePage.niedrigsterParfumSelection();
+        if (alert().present()){
+            alert().dismiss();
+        }
         addToKartPage.addToCartClick();
         addToKartPage.viewCartClick();
         assertTrue(warenkorbPage.checkIftheItemAddedExists(itemDetails));
@@ -48,6 +51,9 @@ public class AddNichePerfumeTest extends FluentLeniumTest {
         nischenDueftePage.niedrigsterOptionSelection();
         String itemDetails =  nischenDueftePage.addItemName();
         nischenDueftePage.niedrigsterParfumSelection();
+        if (alert().present()){
+            alert().dismiss();
+        }
         addToKartPage.addToCartClick();
         addToKartPage.viewCartClick();
         assertTrue(warenkorbPage.checkIftheItemAddedExists(itemDetails));
